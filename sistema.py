890760065a5445f52268ae856b93377fc9654edf
andreL11 <andrelub1234@gmail.com>
2021-03-02 08:46:20 -0300

    desenvolvendo as funções necessárias

--- sistema.py
+++ sistema.py
@@ -47,6 +47,43 @@ avaliacoes = {'Ana':
                    'Norbit': 1.0,
                    'Exterminador do Futuro': 4.0}
               }
-print(avaliacoes['Ana'])
-print(avaliacoes['Ana']['Exterminador do Futuro'])
+"""print(avaliacoes['Ana'])
+print(avaliacoes['Ana']['Exterminador do Futuro'])"""
 
+"distância euclidiana"
+
+print(pow(3-3, 2))
+print(pow(3.5-4, 2))
+
+from math import sqrt
+print(sqrt(pow(3-3, 2) + pow(3.5-4, 2)))
+"""distância entre ana e e cláudia, considerando star trek no eixo x e exterminador no eixo y"""
+
+"""funcao distância euclidiana"""
+def euclidiana (user1, user2):
+    similaridade = {}
+    for item in avaliacoes[user1]:
+        if item in avaliacoes[user2]:
+            similaridade[item] = 1
+    if len(similaridade) == 0 : return 0
+
+    soma = sum([pow(avaliacoes[user1][item] - avaliacoes[user2][item], 2)
+                for item in avaliacoes[user1] if item in avaliacoes[user2]])
+    return 1/(1+sqrt(soma))
+
+
+"""######testanto a função##########"""
+print(euclidiana('Leonardo', 'Ana'))
+print(euclidiana('Marcos', 'Claudia'))
+"""###############################"""
+
+"""função que retorna a similaridade entre um usuário e todos os outros"""
+def getSimilares(usuario):
+    similaridade2 = [(euclidiana(usuario, outros), outros)
+                     for outros in avaliacoes if outros != usuario]
+    similaridade2.sort()
+    """ordena em ordem crescente de similaridade"""
+    similaridade2.reverse()
+    return similaridade2
+
+print(getSimilares('Ana'))
